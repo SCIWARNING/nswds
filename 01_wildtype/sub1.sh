cea74f402b44a39da959224c8766dd2c516ab411
SCIWARNING <104052864+SCIWARNING@users.noreply.github.com>
2023-04-24 13:33:42 +0800

    Add files via upload

---
+++ 01_wildtype/sub1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J GROMACS-DCU
+#SBATCH -p xahdtest
+#SBATCH -N 1
+#SBATCH -n 32
+#SBATCH --gres=dcu:4
+#SBATCH --exclusive
+
+module purge
+module load compiler/devtoolset/7.3.1 
+module load mpi/hpcx/gcc-7.3.1 
+module load mathlib/fftw/3.3.8-gcc-7.3.1-single 
+module load compiler/dtk/22.04.2 
+module load anaconda3/5.2.0
+module load gromacs/2022.1-DCU2-hpcx-gcc-7.3.1
+
+gmx_mpi grompp -f em.mdp -c complex_solv.gro -p complex.top -o em.tpr
+#cmd1='gmx_mpi mdrun -v -pin on -nb gpu -deffnm em'
+mpirun --allow-run-as-root -np 4 gmx_mpi mdrun -v -pin on -nb gpu -deffnm em 
+
